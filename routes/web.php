2d244ee6eda47762593b6ac1411b52dd16793bf0
vinckarunia <vinckarunia@gmail.com>
2024-10-24 14:28:45 +0700

    merge from billy-422023018 : add suppliers

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,7 @@ Route::get('/', function () {
 });
 
 //route resource for products
-<<<<<<< HEAD
-Route::resource('suppliers', \App\Http\Controllers\SupplierController::class);
-=======
 Route::resource('/products', \App\Http\Controllers\ProductController::class);
 Route::resource('/suppliers', \App\Http\Controllers\SupplierController::class);
 Route::resource('/transaksi_penjualans', \App\Http\Controllers\TransaksiPenjualanController::class);
 Route::resource('/detail_transaksi_penjualans', \App\Http\Controllers\detailTransaksiPenjualanController::class);
->>>>>>> 99612a18c55ecfd8e7173edc3d41580f6614c02a
