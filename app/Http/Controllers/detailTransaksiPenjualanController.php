bc0ede865f853f62ca2e9d3b3d6962a3becd8d4e
vinckarunia <vinckarunia@gmail.com>
2024-10-24 15:53:19 +0700

    update route & create detail penjualan template

---
+++ app/Http/Controllers/detailTransaksiPenjualanController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Supplier;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage; 
+
+class detailTransaksiPenjualanController extends Controller
+{
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,4 @@ Route::get('/', function () {
 //route resource for products
 Route::resource('/products', \App\Http\Controllers\ProductController::class);
 Route::resource('/suppliers', \App\Http\Controllers\SupplierController::class);
-Route::resource('/transaksi_penjualans', \App\Http\Controllers\TransaksiPenjualanController::class);
 Route::resource('/detail_transaksi_penjualans', \App\Http\Controllers\detailTransaksiPenjualanController::class);
