7e52b37d5f000eaeed5d1b2548d47508a0f44344
vinckarunia <vinckarunia@gmail.com>
2024-10-24 15:35:18 +0700

    merge from feliciano_422023006 : add transaksi penjualan

---
+++ database/migrations/2024_10_22_151325_create_products_table.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_category_id')->nullable()->index();
+            $table->foreignId('supplier_id')->nullable()->index();
+            $table->string('image');
+            $table->string('title');
+            $table->text('description');
+            $table->bigInteger('price');
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_category_name');
+            $table->timestamps();
+        });
+
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_supplier');
+            $table->string('pic_supplier');
+            $table->string('alamat_supplier');
+            $table->string('no_hp_pic_supplier');
+            $table->timestamps();
+        });
+
+        Schema::create('transaksi_penjualans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kasir');
+            $table->timestamp('tanggal_transaksi'); 
+            $table->timestamps(); 
+        });
+
+        Schema::create('detail_transaksi_penjualans', function (Blueprint $table) {
+            $table->id();
+            $table->integer('jumlah_pembelian');
+            $table->foreignId('transaksi_penjualan_id')->nullable()->index();
+            $table->foreignId('product_id')->nullable()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
