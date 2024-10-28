8614ebed216cdb5c22ef24edd4b652f7a27af5dc
mjayy77 <billymj77@gmail.com>
2024-10-28 23:14:23 +0700

    update (adding font)

--- resources/views/suppliers/show.blade.php
+++ resources/views/suppliers/show.blade.php
@@ -6,6 +6,7 @@
     <title>Show Supplier</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,200;0,300;0,400;1,300;1,500;1,800;1,900&family=Poppins:wght@400;600&family=Rancho&display=swap');
         .bg {
             background: #9ba1a7;
         }
